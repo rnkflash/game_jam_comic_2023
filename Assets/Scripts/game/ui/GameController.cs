32e61c57240749afc22db3292e941a90a4596ead
StepanMaksimov <stepan.maximov@mytona.com>
2023-01-08 19:26:29 +0800

    scene controller

---
+++ Assets/Scripts/game/ui/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnWinClicked() {
+        SceneController.Instance.LoadVictoryScene();
+    }
+
+    public void OnLoseClicked() {
+        SceneController.Instance.LoadLoseScene();
+    }
+}
