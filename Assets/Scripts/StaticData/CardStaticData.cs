fcd53a3a78107036a90418388c1f022d94cfe2a0
KirillTsarkov <idomeneydevkalid@gmail.com>
2023-01-08 21:00:06 +0700

    Added CardsData

---
+++ Assets/Scripts/StaticData/CardStaticData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace StaticData
+{
+  [CreateAssetMenu(fileName = "CardData", menuName = "Static Data/Card")]
+  public class CardStaticData : ScriptableObject
+  {
+    public CardTypeId CardTypeId;
+    public int CardId;
+    
+    public Sprite sprite;
+    [TextArea(10, 100)]
+    public string Question;
+
+    [Header("Yes option")]
+    public int YesFood;
+    public int YesFuel;
+    public int YesMoney;
+    public float YesDistance;
+    
+    [Header("No option")]
+    public int NoFood;
+    public int NoFuel;
+    public int NoMoney;
+    public float NoDistance;
+  }
+}
---
+++ Assets/Scripts/StaticData/CardTypeId.cs
@@ -0,0 +1,11 @@
+namespace StaticData
+{
+  public enum CardTypeId
+  {
+    EventCardLv1 = 1,
+    EventCardLv2 = 2,
+    EventCardLv3 = 3,
+    EventCardLv4 = 4,
+    CheckpointCard = 5
+  }
+}
