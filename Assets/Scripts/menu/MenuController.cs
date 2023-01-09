a045f0909e6bd7ad23747602907963bdf336f493
StepanMaksimov <stepan.maximov@mytona.com>
2023-01-10 05:15:47 +0800

    music change track

--- Assets/Scripts/menu/MenuController.cs
+++ Assets/Scripts/menu/MenuController.cs
@@ -7,8 +7,7 @@ public class MenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (!SoundSystem.IsPlayingMusic())
-            SoundSystem.PlayMusic(Sounds.Instance.GetAudioClip("georgian_folk_rap"));
+        SoundSystem.ChangeTrack(Sounds.Instance.GetAudioClip("georgian_folk_rap"));
     }
 
     // Update is called once per frame
