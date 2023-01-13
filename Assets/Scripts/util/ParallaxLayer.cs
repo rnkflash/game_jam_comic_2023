d3f6a82430b32378a16324c94da2d5b64f1f8860
StepanMaksimov <stepan.maximov@mytona.com>
2023-01-14 06:55:20 +0800

    fixed parallax

--- Assets/Scripts/util/ParallaxLayer.cs
+++ Assets/Scripts/util/ParallaxLayer.cs
@@ -7,22 +7,23 @@ public class ParallaxLayer : MonoBehaviour
         private float width;
         private float cameraWidth;
         private Transform _camera;
+        private Vector3 offsetX;
 
         void Awake()
         {
             _camera = Camera.main.transform;
             width = GetComponent<SpriteRenderer>().bounds.size.x;
             var cameraBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, _camera.transform.position.z));
-            cameraWidth = cameraBounds.x;
+            cameraWidth = cameraBounds.x * 2 - 0.5f;
+            offsetX = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         }
 
         void LateUpdate()
         {
+            transform.position = offsetX + Vector3.Scale(new Vector3(_camera.position.x, 0, 0), movementScale);
+
             if (_camera.position.x > transform.position.x + width) {
-                
-                transform.position = new Vector3(_camera.position.x + 19, transform.position.y, transform.position.z);
-            } else {
-                //transform.position = Vector3.Scale(_camera.position, movementScale);
+                offsetX = new Vector3(width + offsetX.x + cameraWidth, offsetX.y, offsetX.z);
             }
         }
 
