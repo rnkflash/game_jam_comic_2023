dd7602db54e2701c13e673252783cbc06aaee821
KirillTsarkov <idomeneydevkalid@gmail.com>
2023-01-14 19:45:57 +0700

    Update animation and main scene

---
+++ Assets/Scripts/game/objects/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace game.objects
+{
+  public class NPC : MonoBehaviour
+  {
+    private void Start()
+    {
+      transform.DOMoveX(100, 200);
+    }
+  }
+}
